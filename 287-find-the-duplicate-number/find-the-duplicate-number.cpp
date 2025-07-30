3867f6bd29025ae6e51700acbc3cf05cfaa6d9e5
VASHU BHATI <bhativashu31@gmail.com>
2025-07-30 23:07:58 +0530

    Time: 27 ms (37.62%) | Memory: 65.1 MB (69.92%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& arr) {
+        int n = arr.size();
+        int i;
+        sort(arr.begin(),arr.end());
+
+        for(int i=0;i<n-1;i++){
+            if(arr[i] == arr[i+1]){
+                return arr[i];
+            }
+            
+            
+        }
+
+        return 0;
+        
+        
+    }
+}; 
