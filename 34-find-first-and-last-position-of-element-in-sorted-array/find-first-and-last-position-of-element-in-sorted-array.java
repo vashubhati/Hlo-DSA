fe829a5c2503c52758f9ac6c64374ec8973aba54
VASHU BHATI <bhativashu31@gmail.com>
2025-08-26 00:01:54 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (29.80%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -2,20 +2,51 @@ class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] res = {-1, -1};
         
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                res[0] = i; // first occurrence
-                break;
-            }
-        }
-        
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (nums[i] == target) {
-                res[1] = i; // last occurrence
-                break;
-            }
-        }
+        res[0] = findFirst(nums, target);
+        res[1] = findLast(nums, target);
         
         return res;
+    }
+    
+    // Find first occurrence using binary search
+    private int findFirst(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int index = -1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (nums[mid] == target) {
+                index = mid;
+                high = mid - 1; // keep searching left side
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return index;
+    }
+    
+    // Find last occurrence using binary search
+    private int findLast(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int index = -1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (nums[mid] == target) {
+                index = mid;
+                low = mid + 1; // keep searching right side
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return index;
     }
 }
