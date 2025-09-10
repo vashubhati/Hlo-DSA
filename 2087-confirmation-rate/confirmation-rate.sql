acd9488a289d43e36322797cd176eec85e2ba722
VASHU BHATI <bhativashu31@gmail.com>
2025-09-11 00:54:20 +0530

    Time: 650 ms (58.13%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,13 +1,6 @@
 # Write your MySQL query statement below
-SELECT 
-    s.user_id,
-    COALESCE(
-        ROUND(
-            SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) * 1.0 /
-            NULLIF(SUM(CASE WHEN c.action IN ('confirmed', 'timeout') THEN 1 ELSE 0 END), 0)
-        , 2)
-    , 0) AS confirmation_rate
-FROM Signups s
-LEFT JOIN Confirmations c
-    ON s.user_id = c.user_id
-GROUP BY s.user_id;
+select s.user_id, round(avg(if(c.action = "confirmed",1,0)),2) as confirmation_rate from 
+signups s
+left join confirmations c
+on s.user_id = c.user_id
+group by s.user_id
