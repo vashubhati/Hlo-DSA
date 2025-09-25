dd547741f109e7ce7ed0db2a7742f4c1bec51f7c
VASHU BHATI <bhativashu31@gmail.com>
2025-09-26 02:06:38 +0530

    Time: 5 ms (65.28%) | Memory: 44.4 MB (17.12%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,22 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
         if (x < 0) return false; // Negative numbers are not palindromes
-
         int original = x;
-        int rev = 0;
-
-        while (x != 0) {
-            int digit = x % 10;
-
-            // Check for overflow before multiplication
-            if (rev < Integer.MIN_VALUE / 10 || rev > Integer.MAX_VALUE / 10) {
-                return false;
-            }
-
-            rev = (rev * 10) + digit;
-            x = x / 10;
+        int reverse = 0;
+        while(x!=0){
+            int digit = 0;
+            digit = x%10;
+            reverse = (reverse*10)+digit;
+            x = x/10;
         }
+        return reverse == original;
 
-        return rev == original;
-    }
+}
 }
